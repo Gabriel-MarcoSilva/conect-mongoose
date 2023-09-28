c08839d5d756f9d2066fbbf9d555c3e05a713c7c
Gabriel-MarcoSilva <gabrielmarkolinosilva14@gmail.com>
2023-09-28 13:53:49 -0300

    chore: colcoando comando no readme

---
+++ database/connection.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose")
+
+const User = process.env.DB_USER
+const Password = process.env.DB_PASS
+
+const Connect = () =>{
+
+    mongoose.connect(`mongodb+srv://${User}:${Password}@cluster0.bgcyiri.mongodb.net/?retryWrites=true&w=majority`)
+    
+    const connection = mongoose.connection  
+
+    connection.on("error", () =>{
+        console.log("erro ao conectar com o banco de dados")
+    })
+
+    connection.on("open", () =>{
+        console.log("sucesso ao conectar com o banco de dados")
+    })
+}
+
+Connect()
+
+module.exports = mongoose
